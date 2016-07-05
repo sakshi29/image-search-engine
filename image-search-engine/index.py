e08d3d42437a50fb563ec13c1f3c255c60937973
sakshi tripathi <sakshi.trp@gmail.com>
2016-07-05 07:56:11 -0700

    Update index.py

--- image-search-engine/index.py
+++ image-search-engine/index.py
@@ -7,7 +7,9 @@ import glob
 cd=ColorDescriptor((8,12,3))
 
 output=open('output1.csv', 'w+')
-for imagepath in glob.glob('D:/image processing/image-search-engine/testimages/*'):
+"""in image path add location of image folder in your computer"""
+for imagepath in glob.glob('image-search-engine/testimages/*'): 
+    
     imageID=imagepath[imagepath.rfind("/")+1:]
     image=cv2.imread(imagepath)
     
