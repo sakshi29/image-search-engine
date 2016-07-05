acf856bec507e184f385558a1abfccb30b811efe
sakshi tripathi <sakshi.trp@gmail.com>
2016-07-05 07:59:23 -0700

    Update main.py

--- image-search-engine/main.py
+++ image-search-engine/main.py
@@ -7,12 +7,14 @@ cd=ColorDescriptor((8,12,3))
 query=cv2.imread('download.jpg')
 
 feature=cd.describe(query)
-searcher=Searcher('D:/image processing/image-search-engine/output1.csv')
+""" csv file location"""
+searcher=Searcher('image-search-engine/output1.csv')
 results=searcher.search(feature)
 
 cv2.imshow('query',query)
 
 for (score,resultid) in results:
-    results=cv2.imread('D:/image processing/image-search-engine'+'/'+resultid)
+    """ image folder location"""
+    results=cv2.imread('image-search-engine'+'/'+resultid)
     cv2.imshow('result',results)
     cv2.waitKey(0)
